0105f7f0d69e259f69c7a56509315281ae59c120
amanabdulla296 <amanmyrat.abdullayev@gmail.com>
2024-02-24 19:13:19 +0100

    change navigation

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About Aman',
+      pageId: 'd9bc4468ddb24c198df8c636ae5e22a0'
+    },
+    {
+      title: 'Get in Touch',
+      pageId: 'fc387a81229146b9ab65abe687a6b623'
+    }
+  ]
 })
