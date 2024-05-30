bfa5ad88d98ce0922db50230179769d15c63e917
Aman Abdullayev <amanmyrat.abdullayev@gmail.com>
2024-05-30 23:54:44 +0200

    Search console verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
+            <meta name="google-site-verification" content="MIrBiIJYFESJovcbFjTjxa7J-6VvzqYqxj20Yxa_O14" />
             <link rel='shortcut icon' href='/favicon.ico' />
             <link
               rel='icon'
