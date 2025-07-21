a484b22cbff00b79af9db1948acf169606a81d9c
Aman Abdullayev <aman.abdullayev@zalando.de>
2025-07-22 00:06:01 +0200

    fix PafgeHeader.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -1,10 +1,8 @@
 import Head from 'next/head'
 
 import type * as types from '@/lib/types'
-import type * as types from '@/lib/types'
 import * as config from '@/lib/config'
 import { getSocialImageUrl } from '@/lib/get-social-image-url'
-
 
 export const PageHead: React.FC<
   types.PageProps & {
@@ -12,8 +10,9 @@ export const PageHead: React.FC<
     description?: string
     image?: string
     url?: string
+    isBlogPost?: boolean
   }
-> = ({ site, title, description, pageId, image, url }) => {
+> = ({ site, title, description, pageId, image, url, isBlogPost = false }) => {
   const rssFeedUrl = `${config.host}/feed`
 
   title = title ?? site?.name
@@ -38,19 +37,6 @@ export const PageHead: React.FC<
         media='(prefers-color-scheme: light)'
         content='#fefffe'
         key='theme-color-light'
-      />
-      <meta name='mobile-web-app-capable' content='yes' />
-      <meta name='apple-mobile-web-app-status-bar-style' content='black' />
-
-      <meta
-        name='theme-color'
-        media='(prefers-color-scheme: light)'
-        content='#fefffe'
-        key='theme-color-light'
-      />
-      <meta
-        name='apple-mobile-web-app-status-bar-style'
-        content='black'
       />
 
       <meta name='robots' content='index,follow' />
@@ -104,28 +90,6 @@ export const PageHead: React.FC<
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
 
-      {/* Better SEO for the blog posts */}
-      {isBlogPost && (
-        <script type='application/ld+json'>
-          {JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'BlogPosting',
-            '@id': `${url}#BlogPosting`,
-            mainEntityOfPage: url,
-            url,
-            headline: title,
-            name: title,
-            description,
-            author: {
-              '@type': 'Person',
-              name: config.author
-            },
-            image: socialImageUrl
-          })}
-        </script>
-      )}
-
-      {/* Better SEO for the blog posts */}
       {isBlogPost && (
         <script type='application/ld+json'>
           {JSON.stringify({
